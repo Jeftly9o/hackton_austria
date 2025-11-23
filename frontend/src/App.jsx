69dfc0093338ed7b9b032dfec514a93eadb04a12
jeftly <jeftly9o@gmail.com>
2025-11-23 11:19:46 -0600

    gits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import EmailDashboard from "./components/layout/EmailDashboard";
 import Dashboard from "./pages/resultados";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <EmailDashboard />
       <Dashboard />
     </div>
   );
